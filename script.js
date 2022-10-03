f9864e889c4558c3cf97cceec2795b6ba274af06
Poonam <poonam.chauhan229@gmail.com>
2022-10-03 19:47:38 +0530

    color picker

--- script.js
+++ script.js
@@ -8,4 +8,30 @@ let brushthickness = 7;
 const erase = () => (ctx.globalCompositeOperation = "destination-out");
 
 //set current color
-document.querySelector(".color-box").style.backgroundColor = color;
+document.querySelector(".color-btn div").style.backgroundColor = color;
+console.log(document.querySelector(".color-btn"))
+
+
+// getElementsByClassName() returns an HTMLCollection not an Array. You have to convert it into a JavaScript array first :
+// This is because querySelectorAll returns a NodeList (which is a collection of objects).
+// const elems = document.querySelectorAll('select option:checked');
+// const values = Array.prototype.map.call(elems, ({ value }) => value);
+function setActiveColor() {
+    document.querySelector(".color-btn div").style.backgroundColor = color;
+    ctx.strokeStyle = color;
+    ctx.globalCompositeOperation = "source-over";
+  }
+function setColor(){
+    var pallete=document.querySelectorAll('.color')
+    console.log(pallete)
+    Array.prototype.map.call(pallete,(element)=>{
+        // console.log(element)
+        // fetching the color code
+        element.addEventListener('click',()=>{
+            color=element.getAttribute("style").split("--set-color:")[1]
+            console.log(color)
+            setActiveColor()
+        })
+    })
+}
+setColor()
